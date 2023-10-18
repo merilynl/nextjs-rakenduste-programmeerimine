60ad2f1edb97062152e9ac1d04005a5690f89d4f
merilynl <merilynl@tlu.ee>
2023-10-18 12:14:03 +0300

    11.10 classwork 2nd ver

---
+++ app/api/todo/route.jsx
@@ -0,0 +1,9 @@
+import { NextResponse } from 'next/server'
+
+export async function GET() {
+  return NextResponse.json(todos)
+}
+export async function POST(req){
+    const data = await req.json()
+    return NextResponse.json(data.hello)
+}
